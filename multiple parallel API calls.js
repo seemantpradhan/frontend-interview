503b59fb78c58ac24748f156447a27272135574d
Seemant Kumar <73223884+seemantpradhan@users.noreply.github.com>
2024-06-11 03:43:33 +0530

    Update multiple parallel API calls.js

--- multiple parallel API calls.js
+++ multiple parallel API calls.js
@@ -1,10 +1,11 @@
-// Create an application that consumes the https://swapi.py4e.com/api/people  API and displays a table containing a list of Star Wars characters and the films they have appeared in and the vehicles they have used. The application should make multiple parallel API calls to fetch the film and vehicle details using the URLs returned in the first API call and display them in the table.
+// Create an application that consumes the https://swapi.py4e.com/api/people  API 
+// and displays a table containing a list of Star Wars characters and the films they have appeared in 
+// and the vehicles they have used. The application should make multiple parallel API calls to fetch the film 
+// and vehicle details using the URLs returned in the first API call and display them in the table.
 
 import React from 'react';
 import './style.css';
 // import axios from 'axios';
-
-
 
 export default function App() {
   const [persons, setPersons] = React.useState([]);
@@ -51,9 +52,20 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
+      <table>
+        <tr>
+          <th>Actor</th>
+          <th>Films appeared in</th>
+          <th>Vehicles Used</th>
+        </tr>
+        {persons.map((person) => (
+          <tr>
+            <td>{person.name}</td>
+            <td key={person.name}> {person.filmNames.join(', ')}</td>
+            <td key={person.created}> {person.vehicleNames.join(', ')}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
